f6fe083257bbb1e184d7fc6a617279326ef6b9bb
jeendale <jeekoo7203@gmail.com>
2024-09-29 17:43:41 +0900

    init

---
+++ OemoChat/src/main/java/com/jeendale/oemochat/OemoChatApplication.java
@@ -0,0 +1,13 @@
+package com.jeendale.oemochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OemoChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OemoChatApplication.class, args);
+    }
+
+}
---
+++ OemoChat/src/test/java/com/jeendale/oemochat/OemoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeendale.oemochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OemoChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
