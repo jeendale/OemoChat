c9db67297d586ebe8b1972ca1c3f93793f71ad45
jeendale <jeekoo7203@gmail.com>
2024-09-30 12:55:18 +0900

    깃허브 업로드용

---
+++ OemoChat/src/main/java/com/jeendale/oemochat/chat/controller/ChatController.java
@@ -0,0 +1,13 @@
+package com.jeendale.oemochat.chat.controller;
+
+import com.jeendale.oemochat.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/chat")
+public class ChatController {
+    private final ChatService chatService;
+}
---
+++ OemoChat/src/main/java/com/jeendale/oemochat/chat/service/ChatService.java
@@ -0,0 +1,4 @@
+package com.jeendale.oemochat.chat.service;
+
+public class ChatService {
+}
